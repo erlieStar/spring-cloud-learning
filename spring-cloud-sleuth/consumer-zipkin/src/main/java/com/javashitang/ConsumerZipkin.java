fe7abc95b30c509f30f764a8e350b70db60ef4fb
lilimin <290059742@qq.com>
2020-02-16 00:28:35 +0800

    1.完善zipkin的demo

--- spring-cloud-sleuth/consumer-zipkin/src/main/java/com/javashitang/ConsumerZipkin.java
+++ spring-cloud-sleuth/consumer-zipkin/src/main/java/com/javashitang/ConsumerZipkin.java
@@ -1,9 +1,11 @@
 package com.javashitang;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -11,6 +13,7 @@ import org.springframework.web.client.RestTemplate;
  * @Author: lilimin
  * @Date: 2019/7/31 20:30
  */
+@Slf4j
 @RestController
 @SpringBootApplication
 public class ConsumerZipkin {
@@ -27,7 +30,9 @@ public class ConsumerZipkin {
         return new RestTemplate();
     }
 
+    @GetMapping("hello")
     public String hello() {
+        log.info("start invoke");
         return restTemplate.getForObject("http://localhost:8090/hello", String.class);
     }
 }
--- spring-cloud-sleuth/producer-zipkin/src/main/java/com/javashitang/ProducerZipkin.java
+++ spring-cloud-sleuth/producer-zipkin/src/main/java/com/javashitang/ProducerZipkin.java
@@ -1,13 +1,16 @@
 package com.javashitang;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @Author: lilimin
  * @Date: 2019/7/31 20:30
  */
+@Slf4j
 @RestController
 @SpringBootApplication
 public class ProducerZipkin {
@@ -16,7 +19,9 @@ public class ProducerZipkin {
         SpringApplication.run(ProducerZipkin.class);
     }
 
+    @RequestMapping("hello")
     public String hello() {
+        log.info("new request");
         return "hi zipkin";
     }
 }
